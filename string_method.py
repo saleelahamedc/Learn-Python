12c53eedd7ac9911c1dccc01bb130b15018bec93
Saleel Ahamed C <161419224+saleelahamedc@users.noreply.github.com>
2024-03-08 15:20:57 +0530

    numbers

---
+++ string_method.py
@@ -0,0 +1,10 @@
+course = "      Bachelor of commerce"
+print(course)
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.find("com"))
+print(course.replace("o", "A"))
+print("com" in course)
+print("accountancy" not in course)

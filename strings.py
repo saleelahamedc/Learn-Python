4247c7f910cd0f40069c4ad2adb9ce265395dc2a
Saleel Ahamed C <161419224+saleelahamedc@users.noreply.github.com>
2024-03-08 11:24:53 +0530

    learn strings

---
+++ strings.py
@@ -0,0 +1,11 @@
+course = "bachelor of comerce"
+print(course)
+character_length = len(course)
+print(character_length)
+
+print(course[0])
+print(course[1])
+print(course[0:8])
+print(course[0:])
+print(course[-2])
+print(course[:15])

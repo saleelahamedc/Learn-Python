e4ba7dbd7eea6fadf45b6297e2c663cc6a9d4eb1
Saleel Ahamed C <161419224+saleelahamedc@users.noreply.github.com>
2024-03-08 19:50:06 +0530

    ternary operator

--- conditional_statements.py
+++ conditional_statements.py
@@ -1,9 +1,20 @@
-temparatue = int(input("weather: "))
-if temparatue < 20:
+# temparature = int(input("weather: "))
+temparature = 50
+if temparature < 20:
     print("it is cold")
     print("cover up and snuggle")
-elif temparatue > 35:
+elif temparature > 35:
     print("go and drink water")
     print("dont go outside")
 else:
     print("good weather")
+
+
+age = 25
+if age > 25:
+    print("not eligible")
+else:
+    print("you are eligible")
+
+message = "not eligible" if age > 25 else "you are eligible"
+print(message)

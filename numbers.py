fabf83695226008f0af8eefeebc3ca8550430f2b
Saleel Ahamed C <161419224+saleelahamedc@users.noreply.github.com>
2024-03-08 15:55:02 +0530

    numbers

--- numbers.py
+++ numbers.py
@@ -1,3 +1,5 @@
+import math
+
 print(10 + 3)
 print(10 - 3)
 print(10 * 3)
@@ -5,3 +7,12 @@ print(10 / 3)
 print(10 // 3)
 print(10 % 3)
 print(10 ** 3)
+
+
+# math module
+
+
+print(round(2.9))
+print(abs(-5.7))
+print(math.ceil(5.8))
+print(math.floor(5.8))

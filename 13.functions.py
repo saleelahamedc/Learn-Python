f2002096d99883bd1ea79fa4036b0d987ca38ce0
Saleel Ahamed C <161419224+saleelahamedc@users.noreply.github.com>
2024-03-10 15:22:02 +0530

    functions, return

--- 13.functions.py
+++ 13.functions.py
@@ -1,6 +1,19 @@
-def health_quotes():
-    print("smoking causes cancer")
-    print("Dont drink and drive")
+# def health_quotes():
+# print("smoking causes cancer")
+# print("Dont drink and drive")
 
 
-health_quotes()
+# health_quotes()
+
+
+# arguments & paramenters
+
+def health_quotes(first_habit, end_result):
+    return (f"{first_habit} causes {end_result}")
+
+
+print(health_quotes("smoking", "cancer"))
+health_message = health_quotes("Drinking", "Death")
+print(health_message)
+file = open("health_msg.md", "w")
+file.write(health_message)
